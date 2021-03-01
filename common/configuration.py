475f54c2df36769bc8838f378a2c2ed137efc7be
Mohammad Reza Saleh <mr.salehsedghpour@gmail.com>
2021-03-01 13:24:13 +0100

    function for read config file

--- common/configuration.py
+++ common/configuration.py
@@ -7,11 +7,21 @@ from common import utils
 class ConfigManager(ConfigParser):
     """Configuration Manager"""
 
-    def __init__(self):
-        """Initialize """
-        super(ConfigManager, self).__init__()
+    def _read_config(self):
+        """Read Configuration file"""
+        if os.path.exists(self._configfile):
+            try:
+                ConfigManager.read(self, self._configfile)
+            except MissingSectionHeaderError:
+                return "Error Missing Section header in config file: {}".format(self._configfile)
+            except ParsingError:
+                return "Error in parsing config file: {}".format(self._configfile)
 
-        app_dir = utils.app_dir()
-        self._configfile = "{}/config.ini".format(app_dir)
-        self._init_config_file()
-        self._read_config()
+    def save(self):
+        """Write configuration file"""
+        with open(self._configfile, 'w') as fd:
+            try:
+                self.write(fd)
+                fd.close()
+            except IOError:
+                return "Unable to write file on disk."
