6bc806f2e803037bd72b1df95050fafa8ec9af16
Mohammad Reza Saleh <mr.salehsedghpour@gmail.com>
2021-03-01 13:25:11 +0100

    function for initialize config manager

--- common/configuration.py
+++ common/configuration.py
@@ -7,6 +7,15 @@ from common import utils
 class ConfigManager(ConfigParser):
     """Configuration Manager"""
 
+    def __init__(self):
+        """Initialize """
+        super(ConfigManager, self).__init__()
+
+        app_dir = utils.app_dir()
+        self._configfile = "{}/config.ini".format(app_dir)
+        self._init_config_file()
+        self._read_config()
+
     def _init_config_file(self):
         """Initialize Base Config file"""
         if not os.path.exists(self._configfile):
