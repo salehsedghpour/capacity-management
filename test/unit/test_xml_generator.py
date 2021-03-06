d49babd7dcb809a7b3201af8b60084ec181ad586
Mohammad Reza Saleh <mr.salehsedghpour@gmail.com>
2021-03-06 20:38:51 +0100

    test/set wrong domain type

--- test/unit/test_xml_generator.py
+++ test/unit/test_xml_generator.py
@@ -1,6 +1,7 @@
 import pytest
 
 from common.configuration import LibvirtXMLGenerator
+from common import exceptions
 
 
 LibvirtXML = LibvirtXMLGenerator()
@@ -10,3 +11,8 @@ class TestLibvirtXMLGenerator(object):
     def test_set_domain_type(self):
         LibvirtXML.set_domain_type("kvm")
         assert "kvm" == LibvirtXML.domain.get("type")
+
+    def test_set_domain_type_error(self):
+        with pytest.raises(exceptions.InvalidDomainType):
+            LibvirtXML.set_domain_type("test")
+
