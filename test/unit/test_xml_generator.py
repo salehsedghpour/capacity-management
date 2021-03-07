8a7dc453fed8c608559c8a93cd35d171c300e284
Mohammad Reza Saleh <mr.salehsedghpour@gmail.com>
2021-03-07 13:16:23 +0100

    test/set domain vcpu static placement and cpuset

--- test/unit/test_xml_generator.py
+++ test/unit/test_xml_generator.py
@@ -53,6 +53,10 @@ class TestLibvirtXMLGenerator(object):
         LibvirtXML.set_domain_vcpu_placement("auto")
         assert "auto" == LibvirtXML.domain_vcpu.get("placement")
 
+    def test_set_domain_vcpu_placement_cpuset(self):
+        LibvirtXML.set_domain_vcpu_placement("static", "1")
+        assert "1" == LibvirtXML.domain_vcpu.get("cpuset")
+
     def test_set_domain_vcpu_placement_with_wrnong_placement(self):
         with pytest.raises(exceptions.InvalidVCPUPlacement):
             LibvirtXML.set_domain_vcpu_placement("test")
